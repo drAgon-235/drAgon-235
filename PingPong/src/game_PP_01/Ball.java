7507977791d9ab9fdb9d3caa562e559d91e5bf1d
drAgon-235 <81158877+drAgon-235@users.noreply.github.com>
2021-08-02 20:37:41 +0200

    Add files via upload

---
+++ PingPong/src/game_PP_01/Ball.java
@@ -0,0 +1,50 @@
+package game_PP_01;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle
+{	
+	Random random;
+	int xVelocity;
+	int yVelocity;
+	int innitailSpeed = 2;
+	
+	Ball(int x, int y, int width, int height)
+	{
+		super(x, y, width, height);
+		random = new Random();
+		
+		int randomXDirection = random.nextInt(2);
+		if(randomXDirection == 0)
+			randomXDirection--;
+		setXDirection(randomXDirection*innitailSpeed);
+		
+		int randomYDirection = random.nextInt(2);
+		if(randomYDirection == 0)
+			randomYDirection--;
+		setYDirection(randomYDirection*innitailSpeed);
+	}
+	
+	public void setXDirection(int randomXDirection)
+	{
+		xVelocity = randomXDirection;
+	}
+	public void setYDirection(int randomYDirection)
+	{
+		yVelocity = randomYDirection;
+	}
+	public void move()
+	{
+		x += xVelocity;
+		y += yVelocity;
+
+	}
+	public void draw(Graphics g)
+	{
+		g.setColor(Color.white);
+		g.fillOval(x, y, height, width );
+	}
+	
+}
---
+++ PingPong/src/game_PP_01/Start_PongGame.java
@@ -0,0 +1,18 @@
+package game_PP_01;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Start_PongGame
+{
+	
+	
+	public static void main(String[] args)
+	{
+		GameFrame frame = new GameFrame();
+		
+		
+	}
+
+}
